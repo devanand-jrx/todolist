bcde046f5757b97dae4b5c2391b7646809d2d441
devanand-jrx <devanand.ravindran@edstewm.com>
2023-07-20 16:58:21 +0530

    code changes  to todo list application

--- src/main/java/com/devanand/todolistapplication/contract/TaskResponse.java
+++ src/main/java/com/devanand/todolistapplication/contract/TaskResponse.java
@@ -2,26 +2,36 @@ package com.devanand.todolistapplication.contract;
 
 
 import ch.qos.logback.core.status.Status;
+import com.devanand.todolistapplication.validation.ValidDate;
+import com.devanand.todolistapplication.validation.ValidName;
+import com.devanand.todolistapplication.validation.ValidStatus;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.*;
 
 import java.time.LocalDate;
 
-@Data
+
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskResponse {
 
     private int id;
+
+    @ValidName
     private String name;
+
     private String description;
 
 
     @Enumerated(EnumType.STRING)
+    @ValidStatus
     private Status status;
 
+    @ValidDate
     private LocalDate dueDate;
 
     public enum Status {
