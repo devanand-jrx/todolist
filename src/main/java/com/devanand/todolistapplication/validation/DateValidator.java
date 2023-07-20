27ac0e863dffb75557ca3c3fd5e278fb051b06a1
devanand-jrx <devanand.ravindran@edstewm.com>
2023-07-20 14:58:07 +0530

    date validation added to todo list application

---
+++ src/main/java/com/devanand/todolistapplication/validation/DateValidator.java
@@ -0,0 +1,19 @@
+package com.devanand.todolistapplication.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class DateValidator implements ConstraintValidator<ValidDate, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
+        if (date == null) {
+            return false;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        return date.isAfter(currentDate);
+    }
+}
+
---
+++ src/main/java/com/devanand/todolistapplication/validation/ValidDate.java
@@ -0,0 +1,19 @@
+package com.devanand.todolistapplication.validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = DateValidator.class)
+public @interface ValidDate {
+    String message() default "Invalid date format or date is in the past";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
